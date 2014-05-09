c76c4bab25753808bee902488693ef03115e8d1b
Nick O'Neill <nick.oneill@gmail.com>
2014-05-09 14:05:29 -0700

    Update script so I don't have to remember stuff.

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# send everything in public to the root of blog
+# a: archive mode
+#		r: recursive
+#		l: keep symlinks
+#		p: keep perms
+#		t: keep time
+#		g: keep groups
+#		o: keep owner
+#		D: keep devices, specials
+# v: verbose
+# z: compress
+rsync -avz public/* 192.241.193.210:/srv/www/blog.nickoneill.name/public_html/
